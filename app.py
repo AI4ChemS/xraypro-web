be54d5c414e3b5896cdb172b855202850891d553
sartaajkhan <67485363+sartaajkhan@users.noreply.github.com>
2025-07-02 20:27:54 -0400

    Update app.py

--- app.py
+++ app.py
@@ -197,7 +197,7 @@ def recommend(model, loader):
     uptakeLP = predictions_test[0][4]
     bandGap = predictions_test[0][5]
 
-    percentage = 20
+    percentage = 17
 
     recommendationClass['CH4 storage'] = f"Promising ({np.round(uptakeHP*(1 + percentage/100), 2)} mol/kg)" if uptakeHP*(1 + percentage/100) >= thresholds['CH4Storage'] else f"Not Promising ({np.round(uptakeHP*(1 + percentage/100), 2)} mol/kg)"
     recommendationClass['H2 storage'] = f"Promising ({np.round(h2Cap*(1 + percentage/100), 2)} g/L)" if h2Cap*(1 + percentage/100) >= thresholds['H2Storage'] else f"Not Promising ({np.round(h2Cap*(1 + percentage/100), 2)} g/L)"
